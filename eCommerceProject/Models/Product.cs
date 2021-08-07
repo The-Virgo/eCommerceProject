31ac7dfd1a78a831271908fe86717c7e66eae3ce
Jess Martin <jessicaprince360@gmail.com>
2021-08-06 20:54:48 -0700

    Created Product model

---
+++ eCommerceProject/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceProject.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// A salable product
+        /// </summary>
+
+        [Key] // Make Primary Key in DB
+        public int ProductID { get; set; }
+
+        /// <summary>
+        /// Consumer facing name of product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Retail price as US currency
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Category product falls under. Ex. Electronics, furniture, etc.
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
