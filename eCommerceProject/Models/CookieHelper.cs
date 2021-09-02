08deec7d82cc74c475a9c52708f3fb033d5d9274
Jess Martin <jessicaprince360@gmail.com>
2021-09-02 00:30:05 -0700

    Display cart item count in navigation

--- eCommerceProject/Models/CookieHelper.cs
+++ eCommerceProject/Models/CookieHelper.cs
@@ -54,7 +54,8 @@ namespace eCommerceProject.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
